c986c63b203a700b3ea12dbf9c29dd1b0c067aac
Markus Thielker <mail.markus.thielker@gmail.com>
2023-09-15 20:51:33 +0200

    ORY-11: add shell deployment scripts

---
+++ authentication/deploy.sh
@@ -0,0 +1,22 @@
+username=markus
+server=server.thielker.xyz
+path="~/xyz/accounts"
+image=ory-auth-test
+environment=test
+version=${1:-"0.1.0"}
+platform=linux/amd64
+
+# build image and export to tar
+docker build --platform "$platform" --no-cache -t "$image":"$version" .
+docker image save -o "$image"-"$version".tar "$image":"$version"
+gzip "$image"-"$version".tar
+
+# copy image to server and load it
+scp "$image"-"$version".tar.gz "$username"@"$server":"$path"/"$image"-"$version".tar.gz
+scp -r ../docker/ory-"$environment"/* "$username"@"$server":"$path"
+
+# execute remote script on server
+ssh "$username"@"$server" "bash -s" < ./deploy-remote.sh "$image" "$version" "$path"
+
+# clean up files
+rm -rf "$image"-"$version".tar.gz
