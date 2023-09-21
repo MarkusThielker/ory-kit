7b7f2b051e390525787a06c7db41e409017ef281
Markus Thielker <mail.markus.thielker@gmail.com>
2023-09-21 04:59:21 +0200

    ORY-11: remove oauth test from deployed script

--- docker/ory-test/hydra-setup.sh
+++ docker/ory-test/hydra-setup.sh
@@ -5,17 +5,9 @@ code_client=$(docker compose exec hydra \
     --response-type code,id_token \
     --format json \
     --scope openid --scope offline \
-    --redirect-uri https://accounts.thielker.xyz/flow/login \
-    --redirect-uri https://accounts.thielker.xyz/flow/consent \
-    --redirect-uri http://127.0.0.1:5555/callback)
+    --name OAuth2 Client \
+    --redirect-uri https://git.thielker.xyz \
+    --redirect-uri https://finances.thielker.xyz \
+    --redirect-uri https://accounts.thielker.xyz)
 
-code_client_id=$(echo $code_client | jq -r '.client_id')
-code_client_secret=$(echo $code_client | jq -r '.client_secret')
-
-docker compose exec hydra \
-    hydra perform authorization-code \
-    --client-id $code_client_id \
-    --client-secret $code_client_secret \
-    --endpoint https://accounts.thielker.xyz/hydra \
-    --port 5555 \
-    --scope openid --scope offline
+echo $code_client
