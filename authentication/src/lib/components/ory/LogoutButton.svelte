90d059565366b6510a4bbae016f8d0c041e51098
Markus Thielker <mail.markus.thielker@gmail.com>
2023-09-28 08:15:46 +0200

    ORY-25: create component

---
+++ authentication/src/lib/components/ory/LogoutButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { frontendApi } from "$lib/ory";
+    import { t } from "$lib/i18n";
+</script>
+
+{#await frontendApi.createBrowserLogoutFlow() then flow}
+    <a
+        href={flow.data.logout_url}
+        data-sveltekit-preload-data="tap"
+        class="btn-secondary w-full"
+    >
+        {$t("page.logout.title")}
+    </a>
+{/await}
