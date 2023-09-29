82b8adf27382004ea800cc9a19e6709d1b496411
Markus Thielker <mail.markus.thielker@gmail.com>
2023-09-29 13:17:21 +0200

    ORY-31: add improved text node

--- authentication/src/lib/components/ory/FlowText.svelte
+++ authentication/src/lib/components/ory/FlowText.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
+    import { t } from "$lib/i18n";
     import type { UiNode, UiNodeTextAttributes } from "@ory/client";
 
     export let node: UiNode;
     const attributes = node.attributes as UiNodeTextAttributes;
 </script>
 
-{attributes.text.text}
+{node.meta?.label?.text}
+{#if attributes.text.id === 1050015} <!-- lookup_secret_codes -->
+
+    <div class="grid grid-cols-3 gap-2">
+        {#each attributes.text.context.secrets as code}
+            <code class="bg-neutral-200 dark:bg-neutral-700 rounded-md px-2 py-1 text-center">{ code.id === 1050014 ? $t('page.settings.lookup_secret.used') : code.context.secret}</code>
+        {/each}
+    </div>
+
+{:else if attributes.text.id === 1050006} <!-- totp_secret_key -->
+
+    <code class="bg-neutral-200 dark:bg-neutral-700 rounded-md px-2 py-1 text-center">{attributes.text.text}</code>
+
+{:else}
+
+    {attributes.text.text}
+
+{/if}
