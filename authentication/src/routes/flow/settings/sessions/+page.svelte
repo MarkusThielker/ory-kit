95cce05a9fec7a53a74cb0739f82dcf73c3b427e
Markus Thielker <mail.markus.thielker@gmail.com>
2023-10-04 20:09:54 +0200

    ORY-28: add menu to sessions page

--- authentication/src/routes/flow/settings/sessions/+page.svelte
+++ authentication/src/routes/flow/settings/sessions/+page.svelte
@@ -4,3 +4,15 @@
 <svelte:head>
     <title>{$t("page.settings.title")} - {$t("page.settings.sessions.title")}</title>
 </svelte:head>
+
+<div class="space-y-8">
+
+    <div class="card grid grid-cols-2 text-center">
+
+        <a href="/flow/settings" class="text-xl">{$t("page.settings.profile.title")}</a>
+
+        <a href="/flow/settings/sessions" class="text-xl font-semibold">{$t("page.settings.sessions.title")}</a>
+
+    </div>
+
+</div>
