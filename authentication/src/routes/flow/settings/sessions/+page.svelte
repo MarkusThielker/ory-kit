cef8b5dde45e5aeba3b46cb3c37aec7cdf9eb2fc
Markus Thielker <mail.markus.thielker@gmail.com>
2023-10-05 10:59:39 +0200

    ORY-28: display session information

--- authentication/src/routes/flow/settings/sessions/+page.svelte
+++ authentication/src/routes/flow/settings/sessions/+page.svelte
@@ -42,6 +42,29 @@
         {#await promise then sessions}
             {#each sessions as session}
                 <div class="card space-y-4">
+                    <div class="flex flex-row space-x-4 items-start">
+                        <svg viewBox="0 0 100 100" class="mt-1 w-4 {session.active ? 'fill-cyan-200' : 'fill-neutral-900 dark:fill-white'}" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="50" cy="50" r="50"/>
+                        </svg>
+
+                        {#if session.devices}
+                            {#each session.devices as device}
+                                <div class="w-full">
+                                    <p>{device.ip_address}</p>
+                                    {#if session.authenticated_at}
+                                        <p>{$t("page.settings.sessions.sign_in", { date: new Date(session.authenticated_at).toLocaleString() })}</p>
+                                    {/if}
+                                </div>
+                            {/each}
+                        {/if}
+                        <div class="flex items-end">
+                            <button 
+                                class="btn-primary max-w-fit max-h-fit" 
+                                on:click={() => frontendApi.disableMySession({id: session.id}).then(() => window.location.reload())}>
+                                {$t("page.settings.sessions.revoke")}
+                            </button>
+                        </div>
+                    </div>
                 </div>
             {/each}
         {/await}
